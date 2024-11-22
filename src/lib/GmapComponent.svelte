3400e34ca69ae70f35f6c1f6442c7fa4c79455b1
Shuichi TANAKA <shu.at.kantaro.org@gmail.com>
2024-11-22 18:36:32 +0900

    slot / snippet / render

--- src/lib/GmapComponent.svelte
+++ src/lib/GmapComponent.svelte
@@ -3,29 +3,27 @@
     import { PUBLIC_GOOGLE_MAPS as MAPS_KEY } from "$env/static/public";
     import { Loader } from "@googlemaps/js-api-loader";
 
-    let mapElement: HTMLElement = $state(undefined)
-    let map = $state()
+    let mapElement: HTMLElement | undefined = $state();
+    let map = $state();
 
     onMount(async () => {
         const loader = new Loader({
             apiKey: MAPS_KEY,
             version: "weekly",
         });
-        const { Map } = await loader.importLibrary("maps");
-        const { Marker } = await loader.importLibrary("marker");
 
-        map = new Map(mapElement, {
+        const { Map } = await loader.importLibrary("maps");
+        const { Marker, AdvancedMarkerElement } =
+            await loader.importLibrary("marker");
+
+        map = new Map(mapElement as HTMLElement, {
             center: { lat: -25.344, lng: 131.031 },
             zoom: 3,
         });
 
-        setContext('gmap', {mapElement, Map, Marker})
+        setContext("gmap", { mapElement, Map, Marker, AdvancedMarkerElement });
     });
-
 </script>
 
 <div bind:this={mapElement} style="position: relative; height:100%;"></div>
-
-
-
-
+<slot />
