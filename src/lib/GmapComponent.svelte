cdf371c66ae9f5f8bc308b6242b24b8c866c87c9
Shuichi TANAKA <shu.at.kantaro.org@gmail.com>
2024-11-26 17:44:14 +0900

    マーカーは出た。

--- src/lib/GmapComponent.svelte
+++ src/lib/GmapComponent.svelte
@@ -4,12 +4,17 @@
     import type { Snippet } from "svelte";
     import { PUBLIC_GOOGLE_MAPS as MAPS_KEY } from "$env/static/public";
     import { Loader } from "@googlemaps/js-api-loader";
-    //import Marker from "./Marker.svelte";
 
+    // google map を描画する div エレメント
     let mapElement: HTMLElement | undefined = $state();
 
-    let map = writable()
-    let test = writable(1)
+    /** 
+     * google map マップオブジェクト
+     * 　store にいれることでコンポーネントをまたいで利用する
+     */
+    let map = writable();
+
+    let promise = $state();
 
     type Props = {
         children: Snippet;
@@ -22,22 +27,27 @@
         version: "weekly",
     });
 
-    setContext("gmap", ({loader, map, test}));
+    setContext("gmap", { loader, map });
 
-    onMount(async () => {
-        const { Map } = await loader.importLibrary("maps");
-        $map= new Map(mapElement as HTMLElement, {
-            center: { lat: 35.29147, lng: 137.06225 },
-            zoom: 15,
-            mapId: "my-test-map"
-        });
+    onMount(() => {
+        promise = (async () => {
+            const { Map } = await loader.importLibrary("maps");
+            $map = new Map(mapElement as HTMLElement, {
+                center: { lat: 35.29147, lng: 137.06225 },
+                zoom: 15,
+                mapId: "4b9d08c3166f66cd",
+            });
+            return "ok";
+        })();
     });
 </script>
 
 <div bind:this={mapElement} style="position: relative; height:100%;"></div>
 // デフォルトのスロット
-{#if children}
-    {@render children()}
-{:else}
-    <div>fallback</div>
-{/if}
+{#await promise then}
+    {#if children}
+        {@render children()}
+    {:else}
+        <div>fallback</div>
+    {/if}
+{/await}
--- src/lib/Marker.svelte
+++ src/lib/Marker.svelte
@@ -1,19 +1,13 @@
 <script lang="ts">
     import { onMount, getContext } from "svelte";
 
-    const { loader, map, test }: { loader: any; map: any; test: any } =
-        getContext("gmap");
+    const { loader, map }: { loader: any; map: any } = getContext("gmap");
 
     onMount(async () => {
-        //console.log(test, loader, $map);
         const { AdvancedMarkerElement } = await loader.importLibrary("marker");
-
-        setTimeout(() => {
-            console.log($map);
-            const m = new AdvancedMarkerElement({
-                map:$map,
-                position: { lat: 35.29147, lng: 137.06225 },
-            });
-        }, 2000);
+        const m = new AdvancedMarkerElement({
+            map: $map,
+            position: { lat: 35.29147, lng: 137.06226 },
+        });
     });
 </script>
