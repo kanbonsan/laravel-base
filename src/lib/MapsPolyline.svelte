2fc0042a9a9b2765559b5cac3a9cb71cdbf302ea
Shuichi Tanaka <shu.at.kantaro.org@gmail.com>
2024-12-02 07:38:13 +0900

    polyline出せた

--- src/lib/MapsPolyline.svelte
+++ src/lib/MapsPolyline.svelte
@@ -2,11 +2,20 @@
     import { getContext } from "svelte";
     import { statusbar } from "$lib/store/statusBar.store.js";
     const { loader, map }: { loader: any; map: any } = getContext("gmap");
+
+    import { routePoints } from "$lib/store/points.store.js"
+
     let poly: google.maps.Polyline;
 
     (async function () {
         const { Polyline } = await loader.importLibrary("maps");
-        poly = new Polyline();
-        $statusbar = "poly ready";
+        poly = new Polyline({
+            editable: false,
+            clickable: true,
+            map: $map,
+            path: $routePoints
+        });
+        poly.addListener("mouseover", (ev:google.maps.PolyMouseEvent)=>console.log(ev))
+        
     })();
 </script>
